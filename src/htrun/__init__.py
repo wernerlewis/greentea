a5f221c6d748a7c575165c75d2cba74f370109d0
Werner Lewis <Werner.Lewis@arm.com>
2021-06-22 12:32:31 +0100

    cli: Replace optparse in htrun
    optparse has been deprecated since Python 3.2. htrun CLI is updated to
    use argparse instead.

--- src/htrun/__init__.py
+++ src/htrun/__init__.py
@@ -2,23 +2,20 @@
 # Copyright (c) 2021 Arm Limited and Contributors. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
-
-
-"""! @package htrun
+"""Host Test Runner (htrun).
 
 Flash, reset and  perform host supervised tests on Mbed enabled platforms.
 Write your own programs (import this package) or use 'htrun'
 command line tool instead.
-
 """
 
 import imp
 import sys
-from optparse import OptionParser
-from optparse import SUPPRESS_HELP
+from argparse import ArgumentParser, SUPPRESS
 from . import host_tests_plugins
-from .host_tests_registry import HostRegistry  # noqa: F401
-from .host_tests import BaseHostTest, event_callback  # noqa: F401
+
+from .host_tests_registry import HostRegistry
+from .host_tests import BaseHostTest, event_callback
 
 # Set the default baud rate
 DEFAULT_BAUD_RATE = 9600
@@ -29,6 +26,14 @@ DEFAULT_BAUD_RATE = 9600
 
 
 def get_plugin_caps(methods=None):
+    """Get all available plugins.
+
+    Args:
+        methods: If None, get plugins for CopyMethod and ResetMethod.
+
+    Returns:
+        Dictionary with available plugin capabilities for each method.
+    """
     if not methods:
         methods = ["CopyMethod", "ResetMethod"]
     result = {}
@@ -38,13 +43,14 @@ def get_plugin_caps(methods=None):
 
 
 def init_host_test_cli_params():
-    """! Function creates CLI parser object and returns populated options object.
-    @return Function returns 'options' object returned from OptionParser class
-    @details Options object later can be used to populate host test selector script.
-    """
-    parser = OptionParser()
+    """Create CLI parser object. and returns populated options object.
 
-    parser.add_option(
+    Returns:
+        ArgumentParser object with options.
+    """
+    parser = ArgumentParser()
+
+    parser.add_argument(
         "-m",
         "--micro",
         dest="micro",
@@ -52,11 +58,11 @@ def init_host_test_cli_params():
         metavar="MICRO",
     )
 
-    parser.add_option(
+    parser.add_argument(
         "-p", "--port", dest="port", help="Serial port of the target", metavar="PORT"
     )
 
-    parser.add_option(
+    parser.add_argument(
         "-d",
         "--disk",
         dest="disk",
@@ -64,7 +70,7 @@ def init_host_test_cli_params():
         metavar="DISK_PATH",
     )
 
-    parser.add_option(
+    parser.add_argument(
         "-t",
         "--target-id",
         dest="target_id",
@@ -72,8 +78,7 @@ def init_host_test_cli_params():
         metavar="TARGET_ID",
     )
 
-    parser.add_option(
-        "",
+    parser.add_argument(
         "--sync",
         dest="sync_behavior",
         default=2,
@@ -85,8 +90,7 @@ def init_host_test_cli_params():
         metavar="SYNC_BEHAVIOR",
     )
 
-    parser.add_option(
-        "",
+    parser.add_argument(
         "--sync-timeout",
         dest="sync_timeout",
         default=5,
@@ -95,7 +99,7 @@ def init_host_test_cli_params():
         metavar="SYNC_TIMEOUT",
     )
 
-    parser.add_option(
+    parser.add_argument(
         "-f",
         "--image-path",
         dest="image_path",
@@ -107,7 +111,7 @@ def init_host_test_cli_params():
         host_tests_plugins.get_plugin_caps("CopyMethod")
     )
 
-    parser.add_option(
+    parser.add_argument(
         "-c",
         "--copy",
         dest="copy_method",
@@ -115,8 +119,7 @@ def init_host_test_cli_params():
         metavar="COPY_METHOD",
     )
 
-    parser.add_option(
-        "",
+    parser.add_argument(
         "--retry-copy",
         dest="retry_copy",
         default=3,
@@ -125,8 +128,7 @@ def init_host_test_cli_params():
         metavar="RETRY_COPY",
     )
 
-    parser.add_option(
-        "",
+    parser.add_argument(
         "--tag-filters",
         dest="tag_filters",
         default="",
@@ -142,14 +144,14 @@ def init_host_test_cli_params():
         host_tests_plugins.get_plugin_caps("ResetMethod")
     )
 
-    parser.add_option(
+    parser.add_argument(
         "-r",
         "--reset",
         dest="forced_reset_type",
         help="Forces different type of reset. " + reset_methods_str,
     )
 
-    parser.add_option(
+    parser.add_argument(
         "-C",
         "--program_cycle_s",
         dest="program_cycle_s",
@@ -158,29 +160,29 @@ def init_host_test_cli_params():
             "Program cycle sleep. Define how many seconds you want wait after "
             "copying binary onto target (Default is 4 second)"
         ),
-        type="float",
+        type=float,
         metavar="PROGRAM_CYCLE_S",
     )
 
-    parser.add_option(
+    parser.add_argument(
         "-R",
         "--reset-timeout",
         dest="forced_reset_timeout",
         default=1,
         metavar="NUMBER",
-        type="float",
+        type=float,
         help=(
             "When forcing a reset using option -r you can set up after reset "
             "idle delay in seconds (Default is 1 second)"
         ),
     )
 
-    parser.add_option(
+    parser.add_argument(
         "--process-start-timeout",
         dest="process_start_timeout",
         default=60,
         metavar="NUMBER",
-        type="float",
+        type=float,
         help=(
             "This sets the maximum time in seconds to wait for an internal "
             "process to start. This mostly only affects machines under heavy "
@@ -188,7 +190,7 @@ def init_host_test_cli_params():
         ),
     )
 
-    parser.add_option(
+    parser.add_argument(
         "-e",
         "--enum-host-tests",
         dest="enum_host_tests",
@@ -197,15 +199,13 @@ def init_host_test_cli_params():
         help="Define directory with local host tests",
     )
 
-    parser.add_option(
-        "",
+    parser.add_argument(
         "--test-cfg",
         dest="json_test_configuration",
         help="Pass to host test class data about host test configuration",
     )
 
-    parser.add_option(
-        "",
+    parser.add_argument(
         "--list",
         dest="list_reg_hts",
         default=False,
@@ -213,8 +213,7 @@ def init_host_test_cli_params():
         help="Prints registered host test and exits",
     )
 
-    parser.add_option(
-        "",
+    parser.add_argument(
         "--plugins",
         dest="list_plugins",
         default=False,
@@ -222,7 +221,7 @@ def init_host_test_cli_params():
         help="Prints registered plugins and exits",
     )
 
-    parser.add_option(
+    parser.add_argument(
         "-g",
         "--grm",
         dest="global_resource_mgr",
@@ -237,14 +236,13 @@ def init_host_test_cli_params():
     try:
         imp.find_module("fm_agent")
     except ImportError:
-        fm_help = SUPPRESS_HELP
+        fm_help = SUPPRESS
     else:
         fm_help = (
             "Fast Model connection, This option requires mbed-fastmodel-agent "
             'module installed, list CONFIGs via "mbedfm"'
         )
-    parser.add_option(
-        "",
+    parser.add_argument(
         "--fm",
         dest="fast_model_connection",
         metavar="CONFIG",
@@ -252,8 +250,7 @@ def init_host_test_cli_params():
         help=fm_help,
     )
 
-    parser.add_option(
-        "",
+    parser.add_argument(
         "--run",
         dest="run_binary",
         default=False,
@@ -261,8 +258,7 @@ def init_host_test_cli_params():
         help="Runs binary image on target (workflow: flash, reset, output console)",
     )
 
-    parser.add_option(
-        "",
+    parser.add_argument(
         "--skip-flashing",
         dest="skip_flashing",
         default=False,
@@ -270,8 +266,7 @@ def init_host_test_cli_params():
         help="Skips use of copy/flash plugin. Note: target will not be reflashed",
     )
 
-    parser.add_option(
-        "",
+    parser.add_argument(
         "--skip-reset",
         dest="skip_reset",
         default=False,
@@ -279,20 +274,20 @@ def init_host_test_cli_params():
         help="Skips use of reset plugin. Note: target will not be reset",
     )
 
-    parser.add_option(
+    parser.add_argument(
         "-P",
         "--polling-timeout",
         dest="polling_timeout",
         default=60,
         metavar="NUMBER",
-        type="int",
+        type=int,
         help=(
             "Timeout in sec for readiness of mount point and serial port of "
             "local or remote device. Default 60 sec"
         ),
     )
 
-    parser.add_option(
+    parser.add_argument(
         "-b",
         "--send-break",
         dest="send_break_cmd",
@@ -304,8 +299,7 @@ def init_host_test_cli_params():
         ),
     )
 
-    parser.add_option(
-        "",
+    parser.add_argument(
         "--baud-rate",
         dest="baud_rate",
         help=(
@@ -315,7 +309,7 @@ def init_host_test_cli_params():
         metavar="BAUD_RATE",
     )
 
-    parser.add_option(
+    parser.add_argument(
         "-v",
         "--verbose",
         dest="verbose",
@@ -324,24 +318,21 @@ def init_host_test_cli_params():
         help="More verbose mode",
     )
 
-    parser.add_option(
-        "",
+    parser.add_argument(
         "--serial-output-file",
         dest="serial_output_file",
         default=None,
         help="Save target serial output to this file.",
     )
 
-    parser.add_option(
-        "",
+    parser.add_argument(
         "--compare-log",
         dest="compare_log",
         default=None,
         help="Log file to compare with the serial output from target.",
     )
 
-    parser.add_option(
-        "",
+    parser.add_argument(
         "--version",
         dest="version",
         default=False,
@@ -356,10 +347,10 @@ def init_host_test_cli_params():
         """Example: htrun -d E: -p COM5 -f "test.bin" -C 4 -c shell -m K64F"""
     )
 
-    (options, _) = parser.parse_args()
+    args = parser.parse_args()
 
     if len(sys.argv) == 1:
         parser.print_help()
         sys.exit()
 
-    return options
+    return args
